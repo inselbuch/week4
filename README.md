cfeda5a8811a78827c3b517d1a0071284f5a751e
Inselbuch, Frank <frank.inselbuch@accenture.com>
2020-05-04 21:34:02 -0500

    for submittal

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 "# week4" 
+
+For week 4 of the data science course "Getting and Cleaning Data" we were tasked with loading and manipulating some data.
+The source data is in a ridiculous format separated into numerous data files just to make this exercise more difficult.
+
+All the source files are available from this URL
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+After unzipping that thing you will get this set of relevant files:
+
+UCI HAR Dataset
+   |
+   +---  activity_labels.txt
+   +---  features.txt
+   +---  test
+            |
+            +---  subject_test.txt
+            +---  X_test.txt
+            +---  y_test.txt
+   +---  train
+            |
+            +---  subject_train.txt
+            +---  X_train.txt
+            +---  y_train.txt
+
+And this is what is in these files:
+
+activity_labels.txt - associates an integer number with six (6) different activities in text
+features.txt - identifies the 561 columns of measurements
+subject_test.txt - a vector of integers indentifying the subject for each of the sets of measurements in the "test" data
+
+X_test.txt - 2947 rows of measurements each with 561 columns
+X_train.txt - 7352 rows of measurements each with 561 columns
+
+y_test.txt - 2947 rows indicating the activity for the corresponding row in the X_test.txt file
+   (and yes, this is a lowercase y whereas the X is uppercase in the corresponding file... I guess that is called tidy?)
+y_train.txt - 7532 rows indicating the activity for the corresponding row in the X_train.txt file
+
+The analysis is performed in the R source code file run_analysis.R which produces the following data sets:
+
+oneDataSet
+
+   contains rows from both the test and train data sets unioned together
+   has the column activity which has been translated from a number to the text label
+   contains columns for subject and activity plus columns containing the word "mean" or the word "std"
+   i left the variable names alone as I thought those were descriptive just as they were thank you
+
+secondDataSet
+
+   average of each variable in oneDataSet grouped by subject and activity
+
+
---
+++ run_analysis.R
@@ -0,0 +1,84 @@
+#
+#
+#  run_analysis.R
+#
+#  getting and cleaning data, week 4, peer-graded assignment
+#
+#  May 4, 2020 - Inselbuch
+#     new module
+#
+#
+#
+rm(list=ls())
+# options(width=105)
+# packageVersion("dplyr")
+
+setwd('c:/users/frank.inselbuch/git/coursera/w4')
+#
+library(dplyr)
+#
+
+# read the activity labels (I was going to hard code this, but...)
+# activities = c('WALKING','WALKING_UPSTAIRS','WALKING_DOWNSTAIRS','SITTING','STANDING','LAYING')
+fn = "./data/UCI HAR Dataset/activity_labels.txt"
+activities <- read.table(fn,header=FALSE,sep="",strip.white=TRUE,
+   col.names=c('code','activity'))
+
+# read the features
+fn = "./data/UCI HAR Dataset/features.txt"
+features <- read.table(fn, col.names = c("n","functions"))
+
+
+# get the test data
+fn = "./data/UCI HAR Dataset/test/X_test.txt"
+testX <- read.delim(fn,header=FALSE,sep="",dec=".",strip.white=TRUE,fill=TRUE,
+   stringsAsFactors=FALSE, col.names = features$functions)
+testX <- tbl_df(testX)
+
+fn = "./data/UCI HAR Dataset/test/Y_test.txt"
+testY <- read.delim(fn,header=FALSE,sep="",dec=".",strip.white=TRUE,fill=TRUE,stringsAsFactors=FALSE)
+
+fn = "./data/UCI HAR Dataset/test/subject_test.txt"
+testSubject <- read.table(fn,header=FALSE,sep="",dec=".",strip.white=TRUE,fill=TRUE,
+   stringsAsFactors=FALSE,col.names=c("subject"))
+
+
+# get the train data
+fn = "./data/UCI HAR Dataset/train/X_train.txt"
+trainX <- read.delim(fn,header=FALSE,sep="",dec=".",strip.white=TRUE,fill=TRUE,
+   stringsAsFactors=FALSE, col.names = features$functions)
+trainX <- tbl_df(trainX)
+
+fn = "./data/UCI HAR Dataset/train/Y_train.txt"
+trainY <- read.delim(fn,header=FALSE,sep="",dec=".",strip.white=TRUE,fill=TRUE,
+   stringsAsFactors=FALSE)
+
+fn = "./data/UCI HAR Dataset/train/subject_train.txt"
+trainSubject <- read.delim(fn,header=FALSE,sep="",dec=".",strip.white=TRUE,fill=TRUE,
+   stringsAsFactors=FALSE)
+
+
+# layer in the activity codes
+testDS <- mutate(testX,activity=testY$V1)
+trainDS <- mutate(trainX,activity=trainY$V1)
+
+# layer in the subject
+testDS <- mutate(testDS,subject=testSubject$V1)
+trainDS <- mutate(trainDS,subject=trainSubject$V1)
+
+# merge the data sets
+oneDataSet <- bind_rows(testDS,trainDS)
+
+# overwrite the activity column by indexing the integer into the string vector
+oneDataSet$activity =activities[oneDataSet$activity,2]
+
+# only pick out columns that contain the word "mean" or "std")
+# filter would be choosing rows
+# select is choosing columns
+# so the third column will be the columns that contain the word mean
+# the fourt column will be the columns that contain te word std
+oneDataSet <- select(oneDataSet,subject,activity,contains("mean"),contains("std"))
+
+secondDataSet <- oneDataSet %>% group_by(subject,activity) %>% summarize_all(mean)
+
+
